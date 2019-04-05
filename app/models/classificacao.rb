24f92a7287ac95cff046fd53f942c3f5ab5b2008
Eduardo <hobold.edu@hotmail.com>
2019-04-04 21:42:55 -0300

    teste

--- app/models/classificacao.rb
+++ app/models/classificacao.rb
@@ -3,5 +3,6 @@ class Classificacao < ApplicationRecord
     validates :nome, :idade_minima, presence: true
     validates :idade_minima, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 18, 
                                             message: "A idade minima deve ser entre 0 e 18 anos" }
-    validates :idade_minima, uniqueness: true
+    validates :idade_minima, uniqueness: true 
+    
 end
