c8f8fe67d270769b19ca93cc0182cae05e399562
erique <guizanoni1234@gmail.com>
2019-04-04 21:46:04 -0300

    teste

--- app/models/classificacao.rb
+++ app/models/classificacao.rb
@@ -4,5 +4,4 @@ class Classificacao < ApplicationRecord
     validates :idade_minima, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 18, 
                                             message: "A idade minima deve ser entre 0 e 18 anos" }
     validates :idade_minima, uniqueness: true 
-    
 end
